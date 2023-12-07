7c57ae07b022264b6c6dc9dab171a1799c193652
AyushByju <87507065+AyushByju@users.noreply.github.com>
2023-12-07 18:09:47 -0500

    Update page.js
    tags

--- Webpage/webpage/src/components/page.js
+++ Webpage/webpage/src/components/page.js
@@ -5,9 +5,25 @@ import companiesData from './companies.json';
 function LoginForm() {
   const [companyInput, setCompanyInput] = useState('');
   const [filteredCompanies, setFilteredCompanies] = useState([]);
+  const [formData, setFormData] = useState({
+    email: '',
+    firstName: '',
+    lastName: '',
+    title: '',
+    topicsOfInterest: '',
+    province: '',
+    city: '',
+    country: '',
+    postalCode: ''
+  });
 
-  // Sample list of companies
-  const companies = ["Example Co", "Epsilon Ltd", "Acme Corp", "Echo Inc", "Elite Services", "Apple Corp", "Byke store"];
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
 
   const handleCompanyInputChange = (event) => {
     const input = event.target.value;
@@ -15,46 +31,73 @@ function LoginForm() {
     setFilteredCompanies(companiesData.filter(company => company.CompanyName.toLowerCase().startsWith(input.toLowerCase())));
   };
 
-
   const handleCompanySelect = (companyName) => {
     setCompanyInput(companyName);
     setFilteredCompanies([]);
-};
+    setFormData(prevState => ({
+      ...prevState,
+      company: companyName
+    }));
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+    console.log(formData);
+  };
 
   return (
     <div className="container">
-        <div className="register-box">
-            <h2>Register</h2>
-            <form>
-                <label htmlFor="reg-email">Email</label>
-                <input type="email" id="reg-email" name="reg-email" required />
+      <div className="register-box">
+        <h2>CFIN Enrollment</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="reg-email">Email</label>
+          <input type="email" id="reg-email" name="email" value={formData.email} onChange={handleInputChange} required />
 
-                <label htmlFor="first-name">First Name</label>
-                <input type="text" id="first-name" name="first-name" required />
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" name="firstName" value={formData.firstName} onChange={handleInputChange} required />
 
-                <label htmlFor="company">Company</label>
-                <input 
-                  type="text" 
-                  id="company" 
-                  name="company" 
-                  value={companyInput} 
-                  onChange={handleCompanyInputChange} 
-                  required 
-                />
-                  {companyInput && filteredCompanies.length > 0 && (
-                      <ul className="company-dropdown">
-                          {filteredCompanies.map((company, index) => (
-                              <li key={index} onClick={() => handleCompanySelect(company.CompanyName)}>{company.CompanyName}</li>
-                          ))}
-                      </ul>
-                  )}
-                <label htmlFor="location">Location</label>
-                <input type="text" id="location" name="location" required />
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" name="lastName" value={formData.lastName} onChange={handleInputChange} required />
 
-                <button type="submit">Create Account</button>
-            </form>
-        </div>
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" value={formData.title} onChange={handleInputChange} />
+
+          <label htmlFor="topics-of-interest">Topics of Interest</label>
+          <input type="text" id="topics-of-interest" name="topicsOfInterest" value={formData.topicsOfInterest} onChange={handleInputChange} />
+
+          <label htmlFor="company">Company</label>
+          <input 
+            type="text" 
+            id="company" 
+            name="company" 
+            value={companyInput} 
+            onChange={handleCompanyInputChange} 
+            required 
+          />
+          {companyInput && filteredCompanies.length > 0 && (
+              <ul className="company-dropdown">
+                  {filteredCompanies.map((company, index) => (
+                      <li key={index} onClick={() => handleCompanySelect(company.CompanyName)}>{company.CompanyName}</li>
+                  ))}
+              </ul>
+          )}
+
+          <label htmlFor="province">Province</label>
+          <input type="text" id="province" name="province" value={formData.province} onChange={handleInputChange} />
+
+          <label htmlFor="city">City</label>
+          <input type="text" id="city" name="city" value={formData.city} onChange={handleInputChange} />
+
+          <label htmlFor="country">Country</label>
+          <input type="text" id="country" name="country" value={formData.country} onChange={handleInputChange} />
+
+          <label htmlFor="postal-code">Postal Code</label>
+          <input type="text" id="postal-code" name="postalCode" value={formData.postalCode} onChange={handleInputChange} />
+
+          <button type="submit">Create Account</button>
+        </form>
+      </div>
     </div>
   );
 }
